8f0656e3aa8d4c686135be2be1e7d08a72120cc5
MatheusThiesen <matheusreis314@gmail.com>
2023-12-20 02:10:45 -0300

    modulo 1

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common'
+
+import { ConfigModule } from '@nestjs/config'
+import { CreateAccountController } from './controller/create-account.controller'
+import { envSchema } from './env'
+import { PrismaService } from './prisma/prisma.service'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
+})
+export class AppModule {}
